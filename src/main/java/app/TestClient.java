2f9fe79f82ade52fc23341543bbd4322903c3b48
Divakar321 <randhidivakar@gmail.com>
2023-11-14 16:49:55 +0530

    this is a message i am doing for fun

---
+++ src/main/java/app/TestClient.java
@@ -0,0 +1,17 @@
+package app;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+public class TestClient {
+    public static void main(String[] args) {
+        SpringApplication.run(TestClient.class, args);
+    }
+}
+
